98dfcfe239e555e901779b9b8c0cf2febb107c04
Robert Bunch <rdbunch@gmail.com>
2018-12-18 13:41:48 -0500

    Add symbol dynamic

--- scripts.js
+++ scripts.js
@@ -5,17 +5,32 @@ $(document).ready(()=>{
     const stockForm = document.querySelector('.stock-form'); 
     console.log(stockForm)
 
-    const url = `https://api.iextrading.com/1.0/stock/goog/chart`
-    // getJSON method takes 2 args:
-    // 1. where to get the JSON
-    // 2. Function to run when I'm back
-    $.getJSON(url,(theDataJSFoundIfAny)=>{
-        console.log(theDataJSFoundIfAny)
-        $('#stock-body').html(`
-            <tr>
-                <td>${theDataJSFoundIfAny[0].open}</td>
-            </tr>
-        `)
-    })
+    $('.stock-form').submit((event)=>{
+        // stop the browser from sending the form to another page
+        event.preventDefault();
+        console.log(event);
 
+        // .val() is jQuery, .value is native JS
+        const symbol = $('#symbol').val();
+        console.log(symbol);
+
+        // our endoint
+        // an endpoint is a web accessible URL, that responds with data
+        const url = `https://api.iextrading.com/1.0/stock/${symbol}/quote`
+        // getJSON method takes 2 args:
+        // 1. where to get the JSON
+        // 2. Function to run when I'm back
+        $.getJSON(url,(theDataJSFoundIfAny)=>{
+            console.log(theDataJSFoundIfAny)
+            $('#stock-body').html(`
+                <tr>
+                    <td>${theDataJSFoundIfAny.symbol}</td>
+                    <td>${theDataJSFoundIfAny.companyName}</td>
+                    <td>${theDataJSFoundIfAny.high}</td>
+                    <td>${theDataJSFoundIfAny.low}</td>
+                    <td>${theDataJSFoundIfAny.change}</td>
+                </tr>
+            `)
+        })
+    })
 });
